f1a9dad9d9556a1f08353dc2f9e0d086c02fdb9d
ReaziS <kost9n98@yandex.ru>
2019-10-02 12:30:40 +0300

    feat: all tests passed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = null;
+        this.min = null;
+        this.item = null;
+
+    }
 
     setRange(min, max) {
-
+       this.min = min;
+       this.max = max;
     }
 
     guess() {
-
+        
+        return  Math.ceil((this.max + this.min)/2);
+        
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min =  this.guess();
     }
 }
+
 
 module.exports = GuessingGame;
